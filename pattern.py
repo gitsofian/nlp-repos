6f71e6534fba6f0143969738f9951fee16dc5d64
Sofiane <sofiane@meriane.dev>
2022-08-14 15:41:02 +0200

    second commit

--- pattern.py
+++ pattern.py
@@ -3,3 +3,17 @@ point1 = (12, 13)
 
 print(point)
 print(point1)
+
+resp1 = {"data": "Data being received!"}
+resp2 = {"data": None}
+resp3 = {"Error": "Error 222!"}
+response = resp3
+
+
+match response:
+    case {"Error": msg}:
+        print(f"Error {msg}")
+    case {"data": data} if data is not None:
+        print(f"Data is received! {data}")
+    case _:
+        print("Other")
